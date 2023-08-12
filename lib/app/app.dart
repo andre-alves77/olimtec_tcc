6c127544b472980bc6fe640c1f98f3743526a8f6
Luan Dias <luanportela2018@gmail.com>
2023-08-12 00:20:46 -0300

    remove

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
 
 import 'pages/home_page.dart';
 import 'theme/color_schemes.g.dart';
