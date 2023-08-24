279ad5d5a01765476ed2c1e336ed60bf96f1f286
Gabriel Rocha <gabriel.rocha.matos2005@gmail.com>
2023-08-24 20:27:13 -0300

    Add empty about us page

---
+++ lib/app/pages/about_us_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AboutUsPage extends StatelessWidget {
+  const AboutUsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
