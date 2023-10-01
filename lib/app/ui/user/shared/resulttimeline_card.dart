9fef8e1b8d3215af12a80bfc1cb0011a6de13c86
Gabriel Rocha <gabriel.rocha.matos2005@gmail.com>
2023-10-01 20:48:36 -0300

    Added Result and Timeline card

---
+++ lib/app/ui/user/shared/resulttimeline_card.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ResultTimelineCard extends StatelessWidget {
+  const ResultTimelineCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
