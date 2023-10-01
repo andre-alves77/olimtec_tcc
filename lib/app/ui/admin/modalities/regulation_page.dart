987a61caa3287f5f9e4de6ef2b4ab028b9dfb94c
Luan Dias <luanportela2018@gmail.com>
2023-10-01 14:33:20 -0300

    Update AddTeamsAdmin Page

--- lib/app/ui/admin/modalities/regulation_page.dart
+++ lib/app/ui/admin/modalities/regulation_page.dart
@@ -1,8 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:syncfusion_flutter_pdfviewer/pdfviewer.dart';
 
-class RegulationPageAdmin extends StatelessWidget {
-  const RegulationPageAdmin({super.key});
+class RegulationPageAdmin extends StatefulWidget {
+   const RegulationPageAdmin({super.key});
+
+  @override
+  State<RegulationPageAdmin> createState() => _RegulationPageAdminState();
+}
+
+class _RegulationPageAdminState extends State<RegulationPageAdmin> {
+    late PdfViewerController _pdfViewerController;
+
+    @override
+  void initState() {
+    _pdfViewerController = PdfViewerController();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -18,146 +31,161 @@ class RegulationPageAdmin extends StatelessWidget {
         ),
         centerTitle: true,
       ),
-      body: Column(
-        mainAxisSize: MainAxisSize.max,
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          Padding(
-            padding: EdgeInsetsDirectional.fromSTEB(0, 10, 0, 0),
-            child: Row(
-              mainAxisSize: MainAxisSize.max,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  'REGULAMENTO ATUAL',
-                  style: TextStyle(
-                    fontFamily: 'Lato',
-                    fontSize: 24,
-                    fontWeight: FontWeight.bold,
+      body: Center(
+        child: FittedBox(
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Padding(
+                padding: EdgeInsetsDirectional.fromSTEB(0, 10, 0, 0),
+                child: Row(
+                  mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    FittedBox(
+                      child: Text(
+                        'REGULAMENTO ATUAL',
+                        style: TextStyle(
+                          fontFamily: 'Lato',
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              FittedBox(
+                child: Row(
+                  mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'MODADLIDADE: BASQUETE MASCULINO',
+                      style: TextStyle(
+                        color: Theme.of(context).colorScheme.onPrimaryContainer,
+                        fontFamily: 'Lato',
+                        fontSize: 20,
+                        fontWeight: FontWeight.normal,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: EdgeInsetsDirectional.fromSTEB(0, 5, 0, 0),
+                child: FittedBox(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    mainAxisSize: MainAxisSize.max,
+                    children: [
+                      Container(
+                        width: 332, 
+                        height: 450,
+                        decoration: BoxDecoration(),
+                        child: SafeArea(
+                          child: SfPdfViewer.network(
+                            'https://fapesp.br/eventos/2013/04/bioma-pantanal/Jose_sabino.pdf',
+                            controller: _pdfViewerController,
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
                 ),
-              ],
-            ),
-          ),
-          Row(
-            mainAxisSize: MainAxisSize.max,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                'MODADLIDADE: BASQUETE MASCULINO',
-                style: TextStyle(
-                  color: Theme.of(context).colorScheme.onPrimaryContainer,
-                  fontFamily: 'Lato',
-                  fontSize: 20,
-                  fontWeight: FontWeight.normal,
+              ),
+              Padding(
+                padding: EdgeInsets.all(8),
+                child: FittedBox(
+                  child: Row(
+                    mainAxisSize: MainAxisSize.max,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Material(
+                      child: Ink(
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.all(Radius.circular(30)),
+                            border: Border.all(width: 1),
+                            color: Colors.green,
+                          ),
+                        child: InkWell(
+                          onTap: () {},
+                          child: Icon(
+                              Icons.add,
+                              size: 48,
+                          ),
+                        ),
+                      ),
+                    ),
+                      Column(
+                        mainAxisSize: MainAxisSize.max,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Padding(
+                            padding: EdgeInsets.only(left: 10),
+                            child: Text(
+                              'ADICIONAR REGULAMENTO',
+                              style: TextStyle(
+                                fontFamily: 'Lato',
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(right: 15),
+                child: FittedBox(
+                  child: Row(
+                    mainAxisSize: MainAxisSize.max,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Material(
+                        child: Ink(
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.all(Radius.circular(30)),
+                              border: Border.all(width: 1),
+                              color: Theme.of(context).colorScheme.error,
+                            ),
+                          child: InkWell(
+                            onTap: () {},
+                            child: Icon(
+                                Icons.remove,
+                                size: 48,
+                            ),
+                          ),
+                        ),
+                      ),
+                      Column(
+                        mainAxisSize: MainAxisSize.max,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Padding(
+                            padding: EdgeInsetsDirectional.fromSTEB(10, 0, 0, 0),
+                            child: Text(
+                              'REMOVER REGULAMENTO',
+                              style: TextStyle(
+                                fontFamily: 'Lato',
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          )
+                        ],
+                      )
+                    ],
+                  ),
                 ),
               ),
             ],
           ),
-          Padding(
-            padding: EdgeInsetsDirectional.fromSTEB(0, 5, 0, 0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              mainAxisSize: MainAxisSize.max,
-              children: [
-                Container(
-                  width: 332,
-                  height: 450,
-                  decoration: BoxDecoration(),
-                  child: SafeArea(
-                    child: SfPdfViewer.network(
-                      'http://www.pdf995.com/samples/pdf.pdf',
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
-          Padding(
-            padding: EdgeInsets.all(8),
-            child: Row(
-              mainAxisSize: MainAxisSize.max,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Material(
-                child: Ink(
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.all(Radius.circular(30)),
-                      border: Border.all(width: 1),
-                      color: Colors.green,
-                    ),
-                  child: InkWell(
-                    onTap: () {},
-                    child: Icon(
-                        Icons.add,
-                        size: 48,
-                    ),
-                  ),
-                ),
-              ),
-                Column(
-                  mainAxisSize: MainAxisSize.max,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Padding(
-                      padding: EdgeInsets.only(left: 10),
-                      child: Text(
-                        'ADICIONAR REGULAMENTO',
-                        style: TextStyle(
-                          fontFamily: 'Lato',
-                          fontSize: 18,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ),
-                  ],
-                )
-              ],
-            ),
-          ),
-          Padding(
-            padding: EdgeInsets.only(right: 15),
-            child: Row(
-              mainAxisSize: MainAxisSize.max,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Material(
-                  child: Ink(
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.all(Radius.circular(30)),
-                        border: Border.all(width: 1),
-                        color: Theme.of(context).colorScheme.error,
-                      ),
-                    child: InkWell(
-                      onTap: () {},
-                      child: Icon(
-                          Icons.remove,
-                          size: 48,
-                      ),
-                    ),
-                  ),
-                ),
-                Column(
-                  mainAxisSize: MainAxisSize.max,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Padding(
-                      padding: EdgeInsetsDirectional.fromSTEB(10, 0, 0, 0),
-                      child: Text(
-                        'REMOVER REGULAMENTO',
-                        style: TextStyle(
-                          fontFamily: 'Lato',
-                          fontSize: 18,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    )
-                  ],
-                )
-              ],
-            ),
-          ),
-        ],
+        ),
       ),
     );
   }
