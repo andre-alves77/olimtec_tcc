02d402160dbfff3e71c01fcc9ece6eecf6ef2668
Vitorian0 <vitorianovinicius7@gmail.com>
2023-12-08 04:29:53 -0300

    modalityrule

--- lib/app/ui/organization/insert_modality_rule_org.dart
+++ lib/app/ui/organization/insert_modality_rule_org.dart
@@ -90,47 +90,48 @@ class InsertRuleOrganization extends ConsumerWidget {
                       onTap: () async {
                         try {
                           String modalityId = "";
-                        await FirebaseFirestore.instance
-                            .collection('modality')
-                            .where('name', isEqualTo: arg!)
-                            .get()
-                            .then((value) {
-                          modalityId = value.docs.first.id;
-                        });
-
-                        FilePickerResult? result =
-                            await FilePicker.platform.pickFiles(
-                          type: FileType.custom,
-                          allowedExtensions: ['pdf'],
-                        );
-
-                        if (result != null) {
-                          PlatformFile file = result.files.first;
-
-                          final now = DateTime.now();
-                          final uniqueFileName =
-                              'meuArquivo${now.year}-${now.month}-${now.day}${now.hour}-${now.minute}-${now.second}.pdf';
-
-                          final storageRef = FirebaseStorage.instance.ref();
-                          final pdfRef =
-                              storageRef.child("modalityRules/$uniqueFileName");
-                          try {
-                            await pdfRef.putData(file.bytes!);
-                          } on FirebaseException catch (e) {}
-
-                          final pdfUrl = await pdfRef.getDownloadURL();
-
-                          final firestoreRef = FirebaseFirestore.instance;
-                          final docRef = firestoreRef
+                          await FirebaseFirestore.instance
                               .collection('modality')
-                              .doc(modalityId);
-                          await docRef.update({'rulesLing': pdfUrl});
-                        } else {
-                          // User canceled the picker
-                        }
-                        CustomSnackBar(message: 'PDF enviado com sucesso', ref: ref);
+                              .where('name', isEqualTo: arg!)
+                              .get()
+                              .then((value) {
+                            modalityId = value.docs.first.id;
+                          });
+
+                          FilePickerResult? result =
+                              await FilePicker.platform.pickFiles(
+                            type: FileType.custom,
+                            allowedExtensions: ['pdf'],
+                          );
+
+                          if (result != null) {
+                            PlatformFile file = result.files.first;
+
+                            final now = DateTime.now();
+                            final uniqueFileName =
+                                'meuArquivo${now.year}-${now.month}-${now.day}${now.hour}-${now.minute}-${now.second}.pdf';
+
+                            final storageRef = FirebaseStorage.instance.ref();
+                            final pdfRef = storageRef
+                                .child("modalityRules/$uniqueFileName");
+                            try {
+                              await pdfRef.putData(file.bytes!);
+                            } on FirebaseException catch (e) {}
+
+                            final pdfUrl = await pdfRef.getDownloadURL();
+
+                            final firestoreRef = FirebaseFirestore.instance;
+                            final docRef = firestoreRef
+                                .collection('modality')
+                                .doc(modalityId);
+                            await docRef.update({'rulesLing': pdfUrl});
+                          } else {
+                            // User canceled the picker
+                          }
+                          CustomSnackBar(
+                              message: 'PDF enviado com sucesso', ref: ref);
                         } catch (e) {
-                          throw CustomSnackBar(message: 'Houve um erro', ref: ref, type: ScaffoldAlert.error);
+                          //throw CustomSnackBar(message: 'Houve um erro', ref: ref, type: ScaffoldAlert.error);
                         }
                       },
                       child: Icon(
@@ -177,23 +178,27 @@ class InsertRuleOrganization extends ConsumerWidget {
                       onTap: () async {
                         try {
                           String modalityId = "";
-                        await FirebaseFirestore.instance
-                            .collection('modality')
-                            .where('name', isEqualTo: arg!)
-                            .get()
-                            .then((value) {
-                          modalityId = value.docs.first.id;
-                        });
+                          await FirebaseFirestore.instance
+                              .collection('modality')
+                              .where('name', isEqualTo: arg!)
+                              .get()
+                              .then((value) {
+                            modalityId = value.docs.first.id;
+                          });
 
-                        final firestoreRef = FirebaseFirestore.instance;
-                        final docRef =
-                            firestoreRef.collection('modality').doc(modalityId);
-                        await docRef.update({"rulesLing": null});
-                        CustomSnackBar(message: 'PDF apagado com sucesso', ref: ref);
+                          final firestoreRef = FirebaseFirestore.instance;
+                          final docRef = firestoreRef
+                              .collection('modality')
+                              .doc(modalityId);
+                          await docRef.update({"rulesLing": null});
+                          CustomSnackBar(
+                              message: 'PDF apagado com sucesso', ref: ref);
                         } catch (e) {
-                          throw CustomSnackBar(message: 'Houve um erro', ref: ref, type: ScaffoldAlert.error);
+                          throw CustomSnackBar(
+                              message: 'Houve um erro',
+                              ref: ref,
+                              type: ScaffoldAlert.error);
                         }
-                        
                       },
                       child: Icon(
                         Icons.remove,
